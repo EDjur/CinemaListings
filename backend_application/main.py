93766f6d01047e3fe422f135a0a3ab3657bf37de
Elias Djurfeldt <eliasdjurfeldt@gmail.com>
2016-09-25 19:21:35 +0100

    Jinja magic and server tests.

--- backend_application/main.py
+++ backend_application/main.py
@@ -1,4 +1,6 @@
-from back_end_requests import cinelist_requests, imdb_requests
+from backend_application.back_end_requests import cinelist_requests, imdb_requests
+from pathlib import Path
+from jinja2 import Environment, FileSystemLoader
 
 
 def create_movie_list():
@@ -13,10 +15,26 @@ def filter_movie_list(movie_list, cutoff=7.5):
     return movie_list
 
 
-if __name__ == '__main__':
+def render_jinja_template(movie_list):
+
+    path_to_static = str(Path(__file__).parents[1]) + "\static"
+    path_to_templates = path_to_static + "\\templates"
+    print(path_to_templates)
+    env = Environment(loader=FileSystemLoader(path_to_templates))
+    template = env.get_template('index.html')
+    output_from_parsed_template = template.render(foo=movie_list)
+    print(output_from_parsed_template)
+
+    # to save the results
+    with open(path_to_static + "\\index.html", "w") as fh:
+        fh.write(output_from_parsed_template)
+
+
+def run_application():
     movie_list = create_movie_list()
-    print("\nResult")
-    print(movie_list)
     filtered_movie_list = filter_movie_list(movie_list)
-    print(filtered_movie_list)
+    render_jinja_template(filtered_movie_list)
+
+if __name__ == '__main__':
+    run_application()
   ##  print("FILTERED FOR > 7.0 IMDB RATING: {}".format(filtered_movie_list))
--- server.py
+++ server.py
@@ -1,21 +1,26 @@
 import os
+from backend_application import main
+
 try:
-  from SimpleHTTPServer import SimpleHTTPRequestHandler as Handler
-  from SocketServer import TCPServer as Server
+    from SimpleHTTPServer import SimpleHTTPRequestHandler as Handler
+    from SocketServer import TCPServer as Server
 except ImportError:
-  from http.server import SimpleHTTPRequestHandler as Handler
-  from http.server import HTTPServer as Server
+    from http.server import SimpleHTTPRequestHandler as Handler
+    from http.server import HTTPServer as Server
 
 # Read port selected by the cloud for our application
 PORT = int(os.getenv('PORT', 8000))
 # Change current directory to avoid exposure of control files
 os.chdir('static')
+try:
+    main.run_application()
+except BaseException:
+    pass
 
 httpd = Server(("", PORT), Handler)
 try:
-  print("Start serving at port %i" % PORT)
-  httpd.serve_forever()
+    print("Start serving at port %i" % PORT)
+    httpd.serve_forever()
 except KeyboardInterrupt:
-  pass
+    pass
 httpd.server_close()
-
