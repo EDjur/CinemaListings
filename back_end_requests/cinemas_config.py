e198e824d61f55f0377dc164e8baa47d3472a7c9
Elias Djurfeldt <eliasdjurfeldt@gmail.com>
2016-09-25 23:22:07 +0100

    Reverted python 2 change.

--- back_end_requests/cinemas_config.py
+++ back_end_requests/cinemas_config.py
@@ -7,7 +7,7 @@ class CinemasConfig:
     def __init__(self, path):
         self.path = path
         if not os.path.isfile(path):
-            raise EnvironmentError("Error: Missing cinemas_full.json file in {}".format(path))
+            raise FileNotFoundError("Error: Missing cinemas_full.json file in {}".format(path))
         with open(path, encoding="utf-8") as cinemas_json:
             self.cinemas = json.load(cinemas_json)
 
