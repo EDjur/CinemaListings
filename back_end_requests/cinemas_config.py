be7620a1bf71b1f4e61e8d28cb412726d7c6e79c
Elias Djurfeldt <eliasdjurfeldt@gmail.com>
2016-09-25 23:09:44 +0100

    Changed FileNotFound error to work with python 2.

--- back_end_requests/cinemas_config.py
+++ back_end_requests/cinemas_config.py
@@ -7,7 +7,7 @@ class CinemasConfig:
     def __init__(self, path):
         self.path = path
         if not os.path.isfile(path):
-            raise FileNotFoundError("Error: Missing cinemas_full.json file in {}".format(path))
+            raise EnvironmentError("Error: Missing cinemas_full.json file in {}".format(path))
         with open(path, encoding="utf-8") as cinemas_json:
             self.cinemas = json.load(cinemas_json)
 
@@ -19,6 +19,7 @@ def create_cinema_config(path=None):
     if not path:
         path = str(Path(__file__).parents[1]) + "\cinemas_example.json"
     global cinema_config
+    print(path)
     if not cinema_config:
         cinema_config = CinemasConfig(path)
         return cinema_config
