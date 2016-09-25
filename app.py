255d0d5ade3ced2266e537e4c20b8f7596b25c94
Elias Djurfeldt <eliasdjurfeldt@gmail.com>
2016-09-25 22:24:58 +0100

    Fixed port issue.

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 # On Bluemix, get the port number from the environment variable VCAP_APP_PORT
 # When running this app on the local machine, default the port to 8080
-port = int(os.getenv('VCAP_APP_PORT', 8080))
+PORT = int(os.getenv('PORT', 8000))
 
 
 @app.route('/')
@@ -16,4 +16,4 @@ def hello_world():
     return render_template('index.html', movie_list=movie_list)
 
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=port)
+    app.run(host='127.0.0.1', port=PORT)
