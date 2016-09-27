6ddb11de3d7140b2af06184f7ea3fcacf49d33ab
Elias Djurfeldt <eliasdjurfeldt@gmail.com>
2016-09-27 14:12:55 +0100

    Addd sorting by cinema name and movie imdb rating.

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
-from flask import Flask, render_template
 import os
+
+from flask import Flask, render_template
+
 from db_connection import fetch_movie_list_from_db, update_db
 
 app = Flask(__name__)
@@ -14,12 +16,20 @@ def setup_db():
     update_db()
 
 
+def sort_by_imdb_rating(cinema_list):
+    for cinema in cinema_list:
+        cinema.listings.sort(key=lambda x: x.imdb_rating if x.imdb_rating else False, reverse=True)
+    cinema_list.sort(key=lambda x: x.name)
+    return cinema_list
+
+
 @app.route('/')
 def index():
-    movie_list = fetch_movie_list_from_db()
+    cinema_list = fetch_movie_list_from_db()
+    cinema_list = sort_by_imdb_rating(cinema_list)
     # optionally filter movie list filter_movie_list(movie_list, 7)
-    return render_template('index.html', movie_list=movie_list)
+    return render_template('index.html', movie_list=cinema_list)
 
 if __name__ == '__main__':
-    setup_db()
+    # setup_db()
     app.run(host=HOST, port=PORT)
