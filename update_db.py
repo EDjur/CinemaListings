61770b0b3d32f186f68f1e1c42648389b68314fe
Elias Djurfeldt <eliasdjurfeldt@gmail.com>
2017-02-04 18:28:00 +0000

    db testing.

--- update_db.py
+++ update_db.py
@@ -1,12 +1,10 @@
 print("UPDATE DB TOP LEVEL")
 #!/usr/bin/env python
 
-
 from db_connection import update_db
 
 
 def run():
-    print("YOLOOOOOOOOOOOO")
     update_db()
     print("DONE UPDATING DB")
 
